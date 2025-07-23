544bef6397905757962099542c93e4fe0998f2f4
besu178 <besuwinner@gmail.com>
2025-07-23 19:08:58 +0300

    finished the basics based on reacts documentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,4 @@
-import { useState } from "react";
-import ShoppingList from "./basic react components/list";
-import Counter from "./basic react components/usestate";
-
-function App() {
-  const [count, setCount] = useState(0);
-  function handleAdd() {
-    setCount(count + 1);
-  }
-  function handleReset() {
-    setCount(0);
-  }
-  return (
-    <>
-      <ShoppingList />
-      <h1>this is an example of usestate usage</h1>
-      <Counter count={count} handleAdd={handleAdd} handleReset={handleReset} />
-      <Counter count={count} handleAdd={handleAdd} handleReset={handleReset} />
-      <Counter count={count} handleAdd={handleAdd} handleReset={handleReset} />
-      <Counter count={count} handleAdd={handleAdd} handleReset={handleReset} />
-    </>
-  );
+export default function App(){
+    return (<h1>Hello react</h1>
+    )
 }
-
-export default App;
